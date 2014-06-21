e9ae4138570c2f5d7d4ffda36a3e69f8397e43e5
wonderful <wonderful199082@126.com>
2014-06-21 20:37:22 +0800

    Fibonacci

--- Numbers/fibonacci.c
+++ Numbers/fibonacci.c
@@ -1,7 +1,23 @@
 #include <stdio.h>
 
+int Fibonacci(int n)
+{
+  if(n == 1) return 1;
+  if(n == 2) return 1;
+
+  if(n >= 3)
+  {
+    return Fibonacci(n-1) + Fibonacci(n-2);
+  }
+}
+
 int main()
 {
-  //TODO finish the fibonacci
+  int n;
+  printf("please input n.\n");
+  scanf("%d", &n);
+  printf("Fibonacci(%d) = %d\n", n, Fibonacci(n));
+
   return 0;
 }
+
