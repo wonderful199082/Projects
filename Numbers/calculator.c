18cedcef7fccd08aaa1ac25565cd1fe356b5b95b
wonderful <wonderful199082@126.com>
2014-06-29 22:17:38 +0800

    Simple Calculator

--- Numbers/calculator.c
+++ Numbers/calculator.c
@@ -3,81 +3,69 @@
 #include <math.h>
 #include <ctype.h>
 
-#define STACK_NUM 1000
+#define STACK_NUM 100
+
+int priority(char a, char b)
+{
+  if(a == '#') return 0;
+
+  if(a == '*' || a == '/')  return 1;
+  if((a == '+'|| a == '-') && (b == '+' || b == '-')) return 1;
+  if((a =='+' || a == '-') && (b == '*' || b == '/')) return 0;
+}
 
 double exec(double n1, double n2, char op)
 {
   switch(op)
   {
-    case '+': return n1 + n2;
-    case '-': return n1 - n2;
-    case '*': return n1 * n2;
-    case '/': return n1 / n2;
+    case '+' : return n1 + n2;
+    case '-' : return n1 - n2;
+    case '*' : return n1 * n2;
+    case '/' : return n1 / n2;
   }
 }
-
-int op_priority(char op1, char op2)
+double calculate(char str[])
 {
-  if(op1 == '*' || op1 == '/') return 1;
+  char stk_op[STACK_NUM];
+  double stk_num[STACK_NUM];
+  int top_num = 0, top_op = 0;
+  size_t i;
 
-  if((op1 == '+' || op1 == '-') && (op2 == '*' || op2 == '/')) return 0;
+  stk_op[top_op++] = '#';
 
-  if((op1 == '+' || op1 == '-') && (op2 == '+' || op2 == '-')) return 1;
-
-  if(op2 == '#') return 1;
-}
-
-float calculate(char str[])
-{
-  int i;
-
-  char stk_operator[STACK_NUM];
-  double stk_number[STACK_NUM];
-  int top1 = 0, top2 = 0;
-
-  stk_operator[top2++] = '#';
-
-  for(i = 0; i < strlen(str); i++)
+  for( i = 0; i < strlen(str); i++)
   {
     if(isdigit(str[i]))
     {
-      stk_number[top1++] = str[i] - 48;
+      stk_num[top_num++] = str[i] - 48;
     }
     else
     {
-      if(!op_priority(str[i], stk_operator[top2-1]))
+      if(priority (stk_op[top_op-1], str[i]) )
       {
-        double n2 = stk_number[--top1];
-        double n1 = stk_number[--top1];
-        char op = stk_operator[--top2];
-        double tmp = exec(n1, n2, op);
-
-        stk_number[top1++] = tmp;
+        double n2 = stk_num[--top_num];
+        double n1 = stk_num[--top_num];
+        char op = stk_op[--top_op];
+        stk_num[top_num++] = exec(n1, n2, op);
       }
-
-      stk_operator[top2++] = str[i];
+      stk_op[top_op++] = str[i];
     }
   }
 
-  while(stk_operator[top2-1] != '#')
+  while(stk_op[top_op-1] != '#')
   {
-    double n2 = stk_number[--top1];
-    double n1 = stk_number[--top1];
-    char op = stk_operator[--top2];
-    double tmp = exec(n1, n2, op);
-
-    stk_number[top1++] = tmp;
+    double n2 = stk_num[--top_num];
+    double n1 = stk_num[--top_num];
+    char op = stk_op[--top_op];
+    stk_num[top_num++] = exec(n1, n2, op);
   }
-
-  return stk_number[0];
+  return stk_num[0];
 }
 
-
-int main(int argc, char* argv[])
+int main(int argc, char*  argv[])
 {
-  double res = calculate(argv[1]);
+  double  res = calculate(argv[1]);
+  printf("%f\n", res);
 
-  printf("%s = %f\n", argv[1], res);
-
-  return 0.0;
+  return 0;
 }
